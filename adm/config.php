5f441cf6477d8e788257f368d03a31f6a349e702
arturooo-ln <artur.chrominski21@gmail.com>
2020-11-09 16:30:38 +0100

    remove and editing file

--- adm/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$conn = mysqli_connect("localhost","root","","demo");
-
-if(!$conn){
-	die("Connection error: " . mysqli_connect_error());	
-}
-?>
--- login.php
+++ login.php
@@ -1,65 +1,73 @@
+<?php
+include('admin/connect.php');
+if (isset($_SESSION['user_id'])) {
+	header("location:index.php");
+}
+$message = '';
+if (isset($_POST["login"])) {
+	$query = "SELECT * FROM register_user WHERE user_email = :user_email";
+	$statement = $connect->prepare($query);
+	$statement->execute(
+		array(
+			'user_email'	=>	$_POST["user_email"]
+		)
+	);
+	$count = $statement->rowCount();
+	if ($count > 0) {
+		$result = $statement->fetchAll();
+		foreach ($result as $row) {
+			if ($row['user_email_status'] == 'verified') {
+				if (password_verify($_POST["user_password"], $row["user_password"])) {
+					$_SESSION['user_id'] = $row['register_user_id'];
+					header("location:index.php");
+				} else {
+					$message = "<label>Złe hasło. Spróbuj ponownie</label>";
+				}
+			} else {
+				$message = "<label>Twój email nie został zweryfikowany. Sprawdź swoją skrzynkę mailową</label>";
+			}
+		}
+	} else {
+		$message = "<label>Zły adres email. Spróbuj ponownie</label>";
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>PHP Login with OTP Authentication</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script src="http://code.jquery.com/jquery.js"></script>
-    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	</head>
-	<body>
-		<br />
-		<div class="container">
-			<h3 align="center">How to Make Initial Avatar Image in PHP After Registration</h3>
-			<br />
 
-			<?php
-			if(isset($_GET["register"]))
-			{
-				if($_GET["register"] == 'success')
-				{
-					echo '
-					<h1 class="text-success">Email Successfully verified, Registration Process Completed...</h1>
-					';
-				}
-			}
-			?>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
 
-			<div class="row">
-				<div class="col-md-3">&nbsp;</div>
-				<div class="col-md-6">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							<h3 class="panel-title">Login</h3>
-						</div>
-						<div class="panel-body">
-							<form method="POST" id="login_form">
-								<div class="form-group" id="email_area">
-									<label>Enter Email Address</label>
-									<input type="text" name="user_email" id="user_email" class="form-control" />
-									<span id="user_email_error" class="text-danger"></span>
-								</div>
-								<div class="form-group" id="password_area" style="display:none;">
-									<label>Enter password</label>
-									<input type="password" name="user_password" id="user_password" class="form-control" />
-									<span id="user_password_error" class="text-danger"></span>
-								</div>
-								<div class="form-group" id="otp_area" style="display:none;">
-									<label>Enter OTP Number</label>
-									<input type="text" name="user_otp" id="user_otp" class="form-control" />
-									<span id="user_otp_error" class="text-danger"></span>
-								</div>
-								<div class="form-group" align="right">
-									<input type="hidden" name="action" id="action" value="email" />
-									<input type="submit" name="next" id="next" class="btn btn-primary" value="Next" />
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
+<body>
+	<div class="container">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4>Login</h4>
 			</div>
-			
+			<div class="panel-body">
+				<form method="post">
+					<?php echo $message; ?>
+					<div class="form-group">
+						<label>Emial</label>
+						<input type="email" name="user_email" class="form-control" required />
+					</div>
+					<div class="form-group">
+						<label>Hasło</label>
+						<input type="password" name="user_password" class="form-control" required />
+					</div>
+					<div class="form-group">
+						<input type="submit" name="login" value="Login" class="btn btn-info" />
+					</div>
+				</form>
+				<a href="register.php">Register</a>
+			</div>
 		</div>
-		<br />
-		<br />
-	</body>
+	</div>
+</body>
+
 </html>
